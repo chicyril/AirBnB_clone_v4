6533cdb0e1bcbe8e93a9eeef4dda4e95702285f0
cyril <cyrildaniels21+git@gmail.com>
2024-05-06 12:02:27 +0100

    add advance task

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -8,6 +8,7 @@ $(function(){
       delete amenities[$(this).data('name')];
     $('div.amenities h4').text(Object.keys(amenities).join(', '));
   });
+
   $.get("http://0.0.0.0:5001/api/v1/status/", data => {
     if (data.status == "OK") {
       $('div#api_status').addClass("available");
@@ -16,31 +17,34 @@ $(function(){
     }
   });
 
-  $.ajax({
-    type: 'POST',
-    url: 'http://localhost:5001/api/v1/places_search',
-    data: '{}',
-    dataType: 'json',
-    contentType: 'application/json',
-    success: function (data) {
-      $('SECTION.places').append(data.map(place => {
-        return `<article>
-                  <div class="title_box">
-                    <h2>${place.name}</h2>
-                    <div class="price_by_night">${place.price_by_night}</div>
-                  </div>
-                  <div class="information">
-                    <div class="max_guest">${place.max_guest} Guests</div>
-                    <div class="number_rooms">${place.number_rooms} Bedrooms</div>
-                    <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
-                  </div>
-                  <div class="description">
-                    ${place.description}
-                  </div>
-                </article>`
-      }));
-    }
-  });
+  const search = (filters = {}) => {
+    $.ajax({
+      type: 'POST',
+      url: 'http://localhost:5001/api/v1/places_search',
+      data: JSON.stringify(filters),
+      //dataType: 'json',
+      contentType: 'application/json',
+      success: function (data) {
+        $('SECTION.places').empty();
+        $('SECTION.places').append(data.map(place => {
+          return `<article>
+                    <div class="title_box">
+                      <h2>${place.name}</h2>
+                      <div class="price_by_night">${place.price_by_night}</div>
+                    </div>
+                    <div class="information">
+                      <div class="max_guest">${place.max_guest} Guests</div>
+                      <div class="number_rooms">${place.number_rooms} Bedrooms</div>
+                      <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
+                    </div>
+                    <div class="description">
+                      ${place.description}
+                    </div>
+                  </article>`
+        }));
+      }
+    });
+  };
 
   $('#search').click(function () {
     const filters = {amenities: Object.keys(nameAmenity)};
